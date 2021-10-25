28a36af460f0c5947abb77ba7fd65a84f63b7984
silentbeautyir <92143302+silentbeautyir@users.noreply.github.com>
2021-10-25 19:47:03 +0330

    Delete homework4-4.js

--- homework4-4/homework4-4.js
+++
@@ -1,7 +0,0 @@
-// let arr1 = [10,[25, 13],[14,[55]],2];
-// arr2 = arr1.flat(Infinity);
-// console.log(arr2);
-let oldArray = [10,[25,13],[14,[55]],2];
-let newArray = Array.prototype.concat.apply([], oldArray);
-let new2Array = Array.prototype.concat.apply([], newArray);
-console.log(new2Array);
